1096fed573cf66fdb669548d1c3c75237c7025dd
YosueBrayam <152433787+YosueBrayam@users.noreply.github.com>
2024-02-16 19:10:46 +0100

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,126 @@
+
+def print_taulell(partida):
+    print("       0   1   2")
+    print(f"   0 | {partida[0][0]} | {partida[0][1]} | {partida[0][2]} |")
+    print(f"   1 | {partida[1][0]} | {partida[1][1]} | {partida[1][2]} |")
+    print(f"   2 | {partida[2][0]} | {partida[2][1]} | {partida[2][2]} |")
+
+
+def ha_guanyat(partid):
+    partida = [[' X ',' X ',' X '],[' X',' X ',' X '],[' X ',' X ',' X ']]
+
+
+# simbol1 = "X"
+# simbol2 = "O"
+# j1 = "Yosue"
+# j2 = "Alejandro"
+# jugador = j1
+
+simbols = ['X','O']
+noms = ['Yosue','Alejandro']
+jugador = 0
+
+partida = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+print_taulell(partida)
+
+while True:
+    print(noms[jugador], "jugues tu")
+    fila = int(input("Quina fila (0,1,2)? "))
+    col = int(input("Quina col (0,1,2)? "))
+
+    partida[fila][col]=simbols[jugador]
+
+    print_taulell(partida)
+
+    if ha_guanyat(partid):
+        print(noms[jugador], "HS GUANYAAAAT!!!!")
+        break
+    if jugador == 0:
+        jugador = 1
+    else:
+        jugador = 0
+    
+
+    
+    
+
+
+
+#import pygame
+''''''
+'''
+
+fitxers
+
+def guarda_historial(j1,j2)
+    open file 
+    genera timestamp (202402161827)
+    write(202402161827-j1-j2-qui guanya)
+    
+def llista_historial()
+    open file()
+    for.... read
+        PRINT 1-    202402161827 ...
+        2.202402161827....
+        3.202402161827
+def recupera_punts(nom)
+    return punts 
+
+def guarda_punts(nom, punts)
+
+def compra_punts(nom)
+    input(targeta)
+    input(caducitat)
+    input(punts) 5
+    if    API---> openurl
+        saldo = recupera_punts(nom)
+        saddo = saldo + punts
+
+        guarda_punts(nom,saldo)
+    else    
+        error
+
+
+
+
+
+
+
+***************************************
+
+3 EN RALLA
+
+mostra menu
+   def print_menu()
+
+
+1-nova partida
+input demana nom j1
+input jug2
+
+recupera saldo
+print saldo
+    nova partida x=yosue (11p) vs O=alejandro (17p)
+       A   B   C
+   1 |   |   |   |
+   2 |   |   |   |
+   3 |   |   |   |
+
+    yosue, jugues tu. Posicio? B2
+
+       A   B   C
+   1 |   |   |   |
+   2 |   | X |   |
+   3 |   |   |   |
+
+   alejandro, jugues tu. Posicio?
+
+
+   def print_taulell(partida)
+
+
+
+
+
+
+'''
