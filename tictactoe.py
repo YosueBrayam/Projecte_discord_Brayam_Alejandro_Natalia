68e7ee39223ea1c5cbd66198309da677e4266f1f
YosueBrayam <152433787+YosueBrayam@users.noreply.github.com>
2024-02-21 19:50:46 +0100

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -4,56 +4,62 @@ def print_taulell(partida):
     print(f"   1 | {partida[1][0]} | {partida[1][1]} | {partida[1][2]} |")
     print(f"   2 | {partida[2][0]} | {partida[2][1]} | {partida[2][2]} |")
 
-def ha_guanyat(partida):
+def ha_guanyat(partida, jugador):
     # Comprovar files i columnes
     for i in range(3):
-        if partida[i][0] == partida[i][1] == partida[i][2] == ' X ':
-            return True  
-        if partida[0][i] == partida[1][i] == partida[2][i] == ' X ':
+        if partida[i][0] == partida[i][1] == partida[i][2] == jugador:
+            return True 
+        if partida[0][i] == partida[1][i] == partida[2][i] == jugador:
             return True 
 
     # Comprovar diagonals
-    if partida[0][0] == partida[1][1] == partida[2][2] == ' X ':
-        return True
-    if partida[0][2] == partida[1][1] == partida[2][0] == ' X ':
-        return True  
+    if partida[0][0] == partida[1][1] == partida[2][2] == jugador:
+        return True 
+    if partida[0][2] == partida[1][1] == partida[2][0] == jugador:
+        return True 
 
     return False
 
+def es_moviment_valid(partida, fila, col):
+    # Comprovar si la casella està buida
+    return partida[fila][col] == ' '
 
-# simbol1 = "X"
-# simbol2 = "O"
-# j1 = "Yosue"
-# j2 = "Alejandro"
-# jugador = j1
-
-simbols = ['X','O']
-noms = ['Yosue','Alejandro']
+simbols = ['X', 'O']
+noms = ['Yosue', 'Alejandro']
 jugador = 0
 
-partida = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+partida = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 print_taulell(partida)
+
+moviments_realitzats = 0
+max_moviments = 3 * 3  # 3x3 table
 
 while True:
     print(noms[jugador], "jugues tu")
     fila = int(input("Quina fila (0,1,2)? "))
     col = int(input("Quina col (0,1,2)? "))
 
-    partida[fila][col]=simbols[jugador]
+    if not es_moviment_valid(partida, fila, col):
+        print("Moviment no vàlid. La casella ja està ocupada.")
+        continue
+
+    partida[fila][col] = simbols[jugador]
+    moviments_realitzats += 1
 
     print_taulell(partida)
 
-    
-
-    if ha_guanyat(partida):
-        print(noms[jugador], "HAS GUANYAAAAT!!!!")
+    if ha_guanyat(partida, simbols[jugador]):
+        print(noms[jugador], "HAS GUANYAT!!!!")
         break
+
+    if moviments_realitzats == max_moviments:
+        print("Joc Empatat! No hi ha més moviments possibles.")
+        break
+
     if jugador == 0:
         jugador = 1
     else:
         jugador = 0
-    
-
 
 '''
 
