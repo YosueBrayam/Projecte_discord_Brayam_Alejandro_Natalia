d951de168223afcde0a54e57ef8b0b1f4419a296
YosueBrayam <152433787+YosueBrayam@users.noreply.github.com>
2024-02-29 16:09:23 +0100

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,25 @@
+# Códigos de formato ANSI para colores del texto
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RESET = '\033[0m'
+
+
+
+#Nombre de jugadores
+
+
+print(f"\033[95mHola jugadors! Benvingut al joc de Tic Tac Toe, on podran guanyar punts per competir.{RESET}")
+
+
+
+jugador1 = input("Jugador 1. Quin és el teu nom?: ")
+jugador2 = input("Jugador 2. Quin és el teu nom?: ") 
+
+#############TAULA################
+
+
+
 def print_taulell(partida):
     print("       0   1   2")
     print(f"   0 | {partida[0][0]} | {partida[0][1]} | {partida[0][2]} |")
@@ -5,14 +27,14 @@ def print_taulell(partida):
     print(f"   2 | {partida[2][0]} | {partida[2][1]} | {partida[2][2]} |")
 
 def ha_guanyat(partida, jugador):
-    # Comprovar files i columnes
+   
     for i in range(3):
         if partida[i][0] == partida[i][1] == partida[i][2] == jugador:
             return True 
         if partida[0][i] == partida[1][i] == partida[2][i] == jugador:
             return True 
 
-    # Comprovar diagonals
+    
     if partida[0][0] == partida[1][1] == partida[2][2] == jugador:
         return True 
     if partida[0][2] == partida[1][1] == partida[2][0] == jugador:
@@ -23,9 +45,10 @@ def ha_guanyat(partida, jugador):
 def es_moviment_valid(partida, fila, col):
     # Comprovar si la casella està buida
     return partida[fila][col] == ' '
+    
 
 simbols = ['X', 'O']
-noms = ['Yosue', 'Alejandro']
+noms = [jugador1 , jugador2]
 jugador = 0
 
 partida = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
